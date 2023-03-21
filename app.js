5cd881da55f5f64327f5f2aa9b4cf8f20a5c5176
Lenni009 <guentherhanslphdyt@gmail.com>
2023-03-21 20:39:17 +0000

    Deploying to gh-pages from @ Lenni009/action-test@da37c68141a72ffebaa33af0a8fdc3f22f636a02 🚀

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-document.getElementById('app').insertAdjacentHTML('afterbegin', 'Baum');
-
-// this is a comment
-
-console.log("ÄÄh nein");
-
-document.getElementById('app').insertAdjacentHTML('beforeend', 1 + 1);
+document.getElementById("app").insertAdjacentHTML("afterbegin","Baum"),console.log("ÄÄh nein"),document.getElementById("app").insertAdjacentHTML("beforeend",2);
