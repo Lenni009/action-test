93bf7446104e1046771f01029ea0ff06f63f9061
Lenni <guentherhanslphdyt@gmail.com>
2023-03-25 15:32:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ document.getElementById('app').insertAdjacentHTML('afterbegin', 'Baum');
 
 console.log("ÄÄh nein");
 
+/**
+ * This function calculates the sum, average, minimum, and maximum of a set of numbers, and formats the results for display on a web page. If no arguments are provided, the function will automatically find and calculate the numbers for all elements with the `numberStats` property.
+ *
+ * @param {HTMLElement} element - The HTML element to calculate statistics for.
+ * @param {number} [decimals=null] - The number of decimal places to use in formatted results. If `null`, the function will use the default number of decimal places.
+ * @param {boolean} [outputRaw=false] - Whether to output the raw calculation values along with the formatted results. If `true`, the function will return an object with `formatted` and `raw` properties; if `false`, it will return only the formatted string.
+ * @returns {string|object} The formatted statistics string (and optionally, the raw calculation values).
+ */
 document.getElementById('app').insertAdjacentHTML('beforeend', 1 + 1);
