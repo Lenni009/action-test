9104cfaf15ffc6ef4c8547dd6f4130decab9386d
Lenni <guentherhanslphdyt@gmail.com>
2023-03-25 18:13:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ console.log("ÄÄh nein");
  * @param {HTMLElement} element - The HTML element to calculate statistics for.
  * @param {number} [decimals=null] - The number of decimal places to use in formatted results. If `null`, the function will use the default number of decimal places.
  * @param {boolean} [outputRaw=false] - Whether to output the raw calculation values along with the formatted results. If `true`, the function will return an object with `formatted` and `raw` properties; if `false`, it will return only the formatted string.
- * @returns {string|object} The formatted statistics string (and optionally, the raw calculation values).
+ * @returns {string|Object} The formatted statistics string (and optionally, the raw calculation values).
  */
 document.getElementById('app').insertAdjacentHTML('beforeend', 1 + 1);
